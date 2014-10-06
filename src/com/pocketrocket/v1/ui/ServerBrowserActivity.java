4bcb3f4e904f078cd30cbf7f71a024a7cfa628a4
Andrew W <andrewjw1995@gmail.com>
2014-10-06 20:11:23 +1300

    Added server browser activity
    Added server browser activity with onCreate method, and appropriate
    string resource and layout file

---
+++ src/com/pocketrocket/v1/ui/ServerBrowserActivity.java
@@ -0,0 +1,24 @@
+package com.pocketrocket.v1.ui;
+
+import com.sneakyrocket.pocketrocket.R;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * ServerBrowserActivity is the activity used to view, add, and remove
+ * a user's known servers.
+ * @author Andrew
+ *
+ */
+public class ServerBrowserActivity extends ActionBarActivity {
+	//TODO: add member variables as classes are made
+	
+		@Override
+		public void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			setTitle(R.string.server_browser_title);
+			setContentView(R.layout.server_browser_layout);
+			//TODO: add list of servers
+		}
+}
