230252ce6fd9be400bad3c9762e3e704cc120e8a
Andrew W <andrewjw1995@gmail.com>
2014-10-06 17:09:31 +1300

    Created ScriptListingActivity class
    Created class to display the list of all scripts.

---
+++ src/com/pocketrocket/v1/ui/ScriptListingActivity.java
@@ -0,0 +1,20 @@
+package com.pocketrocket.v1.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * ScriptListingActivity is the activity a user will see when they first connect
+ * to a server and obtain a list of all the runnable scripts.
+ * @author Andrew
+ *
+ */
+public class ScriptListingActivity extends ActionBarActivity {
+	//TODO: add member variables as classes are made
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		//TODO: Inflate layout
+	}
+}
