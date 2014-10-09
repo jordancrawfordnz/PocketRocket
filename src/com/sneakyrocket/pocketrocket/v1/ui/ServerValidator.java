9cb2081e1b0d1d7cb467df87b70d24a1f20ebbd6
Andrew W <andrewjw1995@gmail.com>
2014-10-09 22:08:45 +1300

    Updated server browser to add servers to list
    Updated the ServerBrowserActivity class to use the generic list adapter
    I made, and to add servers to it as they are created. Also updated the
    ServerValidator class to use the functionality.

--- src/com/sneakyrocket/pocketrocket/v1/ui/ServerValidator.java
+++ src/com/sneakyrocket/pocketrocket/v1/ui/ServerValidator.java
@@ -6,14 +6,17 @@ import com.sneakyrocket.pocketrocket.v1.remote.Server;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
-import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 public class ServerValidator implements OnClickListener {
-	private ServerBrowserActivity browser;
+	private View dialogView;
+	private ServerBrowserActivity target;
+	private Server server = null;
 	
-	public ServerValidator(ServerBrowserActivity browser) {
-		this.browser = browser;
+	public ServerValidator(View dialogView, ServerBrowserActivity target) {
+		this.dialogView = dialogView;
+		this.target = target;
 	}
 
 	@Override
@@ -23,16 +26,22 @@ public class ServerValidator implements OnClickListener {
 	public void onClick(DialogInterface dialog, int which) {
 		switch (which) {
 		case AlertDialog.BUTTON_POSITIVE:
-			TextView nameView = (TextView) browser.findViewById(R.id.serverName);
-			TextView addressView = (TextView) browser.findViewById(R.id.serverAddress);
-			Server server = new Server(nameView.getText().toString(),
-					addressView.getText().toString());
-			browser.addServer(server);
+			TextView nameView = (TextView) dialogView.findViewById(R.id.newServerName);
+			TextView addressView = (TextView) dialogView.findViewById(R.id.newServerAddress);
+			TextView portView = (TextView) dialogView.findViewById(R.id.newServerPort);
+			server = new Server(
+					nameView.getText().toString(),
+					addressView.getText().toString(),
+					Integer.parseInt(portView.getText().toString()));
+			validate();
 			break;
 		case AlertDialog.BUTTON_NEGATIVE:
 			dialog.cancel();
 			break;
 		}
-
+	}
+	
+	public void validate(){
+		target.addServer(server);
 	}
 }
