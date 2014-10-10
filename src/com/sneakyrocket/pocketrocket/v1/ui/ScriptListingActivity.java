5bab1f033efed48602aa5aaee84a58aa4df5c7d6
Andrew W <andrewjw1995@gmail.com>
2014-10-10 21:49:33 +1300

    Updated script list activity
    ScriptListActivity now gets scripts from the session it is created with,
    and displays them on screen.

--- src/com/sneakyrocket/pocketrocket/v1/ui/ScriptListingActivity.java
+++ src/com/sneakyrocket/pocketrocket/v1/ui/ScriptListingActivity.java
@@ -1,9 +1,16 @@
 package com.sneakyrocket.pocketrocket.v1.ui;
 
 import com.sneakyrocket.pocketrocket.R;
+import com.sneakyrocket.pocketrocket.v1.core.Script;
+import com.sneakyrocket.pocketrocket.v1.core.Session;
+import com.sneakyrocket.pocketrocket.v1.ui.server.BasicListAdapter;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 /**
  * ScriptListingActivity is the activity a user will see when they first connect
@@ -12,13 +19,41 @@ import android.support.v7.app.ActionBarActivity;
  *
  */
 public class ScriptListingActivity extends ActionBarActivity {
-	//TODO: add member variables as classes are made
+	private Session session;
+	private BasicListAdapter adapter;
+	private ListView scriptListView;
+	
+	/**
+	 * Creates a new activity to show the list of scripts available in the
+	 * current session.
+	 * @param session the session from which the scripts are to be run
+	 */
+	public ScriptListingActivity(Session session) {
+		this.session = session;
+		adapter = new BasicListAdapter();
+	}
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setTitle(R.string.script_list_title);
 		setContentView(R.layout.script_list_layout);
-		//TODO: add list of scripts
+		scriptListView = (ListView) findViewById(R.id.scriptList);
+		scriptListView.setAdapter(adapter);
+		for (Script script : session.listScripts())
+			addScript(script);
+	}
+	
+	/**
+	 * Adds the given script to the list of scripts, and updates the view
+	 * @param script the script to be added
+	 */
+	public void addScript(Script script) {
+		View serverView = getLayoutInflater().inflate(R.layout.script_layout, scriptListView, false);
+		TextView scriptName = (TextView) serverView.findViewById(R.id.scriptName);
+		ImageView scriptIcon = (ImageView) serverView.findViewById(R.id.scriptIcon);
+		scriptName.setText(script.getName());
+		scriptIcon.setImageBitmap(script.getIcon());
+		adapter.addView(serverView);
 	}
 }
