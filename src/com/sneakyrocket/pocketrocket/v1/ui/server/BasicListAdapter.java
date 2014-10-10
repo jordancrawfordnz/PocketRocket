e24593c48c18a396bd181ce6d66a72d46404a493
Andrew W <andrewjw1995@gmail.com>
2014-10-10 21:14:48 +1300

    Moved server UI classes to specific package
    Moved all server-specific ui classes into a new sub-package within the
    ui package. I expect it would get very cluttered if I left as it was.

---
+++ src/com/sneakyrocket/pocketrocket/v1/ui/server/BasicListAdapter.java
@@ -0,0 +1,94 @@
+package com.sneakyrocket.pocketrocket.v1.ui.server;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.database.DataSetObserver;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListAdapter;
+
+public class BasicListAdapter implements ListAdapter {
+	private List<View> items;
+	private List<DataSetObserver> observers;
+	
+	public BasicListAdapter() {
+		items = new ArrayList<View>();
+		observers = new ArrayList<DataSetObserver>();
+	}
+	
+	public void addView(View view) {
+		items.add(view);
+		for (DataSetObserver observer : observers)
+			observer.onChanged();
+	}
+	
+	public void removeView(View view) {
+		items.remove(view);
+		for (DataSetObserver observer : observers)
+			observer.onChanged();
+	}
+
+	@Override
+	public int getCount() {
+		return items.size();
+	}
+
+	@Override
+	public Object getItem(int position) {
+		return items.get(position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
+
+	@Override
+	public int getItemViewType(int position) {
+		return 0;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		return items.get(position);
+	}
+
+	@Override
+	public int getViewTypeCount() {
+		return 1;
+	}
+
+	@Override
+	public boolean hasStableIds() {
+		return false;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return items.isEmpty();
+	}
+
+	@Override
+	public void registerDataSetObserver(DataSetObserver observer) {
+		observers.add(observer);
+	}
+
+	@Override
+	public void unregisterDataSetObserver(DataSetObserver observer) {
+		observers.remove(observer);
+	}
+
+	@Override
+	public boolean areAllItemsEnabled() {
+		for (View view : items)
+			if (!view.isClickable())
+				return false;
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled(int position) {
+		return items.get(position).isEnabled();
+	}
+}
