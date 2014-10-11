0e26543ada73e95e96235f9c91a1983cdcc7349b
Andrew W <andrewjw1995@gmail.com>
2014-10-11 19:00:13 +1300

    Added class to handle starting a new session
    Added a class that listens for a click event on a view, creates a new
    session, and launches a ScriptListingActivity.

---
+++ src/com/sneakyrocket/pocketrocket/v1/ui/server/ServerClickListener.java
@@ -0,0 +1,36 @@
+package com.sneakyrocket.pocketrocket.v1.ui.server;
+
+import java.net.UnknownHostException;
+
+import com.sneakyrocket.pocketrocket.R;
+import com.sneakyrocket.pocketrocket.v1.core.Session;
+import com.sneakyrocket.pocketrocket.v1.remote.Server;
+import com.sneakyrocket.pocketrocket.v1.ui.ScriptListingActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
+
+public class ServerClickListener implements OnClickListener{
+	private Server server;
+	
+	public ServerClickListener(Server server) {
+		this.server = server;
+	}
+
+	@Override
+	public void onClick(View view) {
+		Context context = view.getContext();
+		
+		try {
+			Session session = new Session(server);
+			Session.setCurrentSession(session);
+			Intent intent = new Intent(context, ScriptListingActivity.class);
+			context.startActivity(intent);
+		} catch(UnknownHostException e) {
+			Toast.makeText(context, R.string.address_unresolvable, Toast.LENGTH_LONG).show();
+		}
+	}
+}
