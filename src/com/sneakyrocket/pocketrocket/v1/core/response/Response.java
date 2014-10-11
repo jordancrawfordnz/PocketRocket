6b045f9ca11c599d2a050792dd4b5a0553554f91
jordancrawfordnz <jordan.crawford@me.com>
2014-10-11 23:31:19 +1300

    Create a new response handler

--- src/com/sneakyrocket/pocketrocket/v1/core/response/Response.java
+++ src/com/sneakyrocket/pocketrocket/v1/core/response/Response.java
@@ -1,79 +1,23 @@
 package com.sneakyrocket.pocketrocket.v1.core.response;
 
-import java.io.InputStream;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.util.HashMap;
-
+import java.io.*;
 
 public abstract class Response {
 	protected String args;
-	protected InputStream inputStream;
+	protected InputStream response;
+	protected OutputStream command;
 	
 	// Args is anything after the ":" in the string
-	public Response(String args, InputStream inputStream)
+	public Response(String args, InputStream response, OutputStream command)
 	{
-		if(args == null || inputStream == null)
+		if(args == null || response == null)
 			throw new IllegalArgumentException();
 		this.args = args;
-		this.inputStream = inputStream;
+		this.response = response;
+		this.command = command;
 	}
-	
-	// TODO Implement as a singleton instead?
-	
-	public static void Handle(String responseLine, InputStream inputStream)
-	{
-		if(responseHandlers == null)
-		{
-			responseHandlers = new HashMap<Integer,  Class<? extends Response>>();
-			responseHandlers.put(100, Done.class);
-			
-		}
-		if(responseLine == null || inputStream == null)
-			throw new IllegalArgumentException();
-		
-		String[] splitResponse = responseLine.split(" ",2);
-		String textCode = splitResponse[0];
-		
-		if(textCode.length() != 3)
-			throw new IllegalArgumentException();
-		// the code is valid
-		
-		int code = Integer.parseInt(textCode);
-		
-		String args = "";
-		if(splitResponse.length > 1)
-		{
-			String toArgsSplit = splitResponse[1];
-			String[] argsSplit = toArgsSplit.split(":", 2);
-			if(argsSplit.length > 1)
-				args = argsSplit[1];
-		}
-		// TODO Change from test args
-		
-		if(responseHandlers.containsKey(code)) // if this is a code we know how to handle
-		{
-			Class<? extends Response> handlerClass = responseHandlers.get(code);
-			try
-			{
-				Constructor<? extends Response> constructHandler = handlerClass.getDeclaredConstructor(String.class,InputStream.class);
-				Response handler = constructHandler.newInstance(args, inputStream);
-				((Done)handler).getArgs();
-			}
-			// If anything went wrong with finding the constructors, don't bother fixing it.
-			catch(NoSuchMethodException ex) {
-			}
-			catch (InstantiationException e) {
-			} 
-			catch (IllegalAccessException e) {
-			}
-			catch (IllegalArgumentException e) {
-			}
-			catch (InvocationTargetException e) {
-			}
-		}
-	}
-	
-	private static HashMap<Integer, Class<? extends Response>> responseHandlers = null;
+
+	// Returns true if end of response chain (and no more lines need handling), false otherwise.
+	public abstract boolean handleResponse() throws IOException;
 
 }
