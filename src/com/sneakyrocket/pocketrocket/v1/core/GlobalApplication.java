2dfe5b5e907d68098830945665e86681a9cf7625
jordancrawfordnz <jordan.crawford@me.com>
2014-10-12 00:04:23 +1300

    Add testing code for fake server

--- src/com/sneakyrocket/pocketrocket/v1/core/GlobalApplication.java
+++ src/com/sneakyrocket/pocketrocket/v1/core/GlobalApplication.java
@@ -1,5 +1,11 @@
 package com.sneakyrocket.pocketrocket.v1.core;
 
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+
+import com.sneakyrocket.pocketrocket.v1.core.response.ResponseHandler;
+
 import android.app.Application;
 import android.content.Context;
 
@@ -19,7 +25,35 @@ public class GlobalApplication extends Application {
 		super.onCreate();
 		singleton = this;
 		context = getApplicationContext();
+		
+		// This would usually be coming from a Command.
+		try
+		{
+			InetAddress myComputer = InetAddress.getByName("192.168.1.101");
+			Socket testingOutput = new Socket(myComputer,5050);
+			Connection fakeServer = new Connection(myComputer,2359);
+			fakeServer.sendRequest("hello");
+			ResponseHandler response = fakeServer.getResponseHandler();
+			InputStream commandResponse =  response.getResponse();
+			copyFile(commandResponse,testingOutput.getOutputStream());
+		}
+		catch(Exception ex)
+		{
+			System.err.println(ex.toString());
+		}
 	}
+	
+	
+	public static void copyFile(InputStream in, OutputStream out) {
+		try {
+			int c;
+			while ((c = in.read()) != -1) {
+				out.write(c);
+			}
+		} catch (IOException e){
+			System.err.println("File copy failed: " + e );
+		}
+	} //copyFile()
 	
 	public Context getApplicationContext() {
 		return context;
