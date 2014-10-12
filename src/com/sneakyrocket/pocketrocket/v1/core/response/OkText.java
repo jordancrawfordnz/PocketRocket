7aa9afcd344f605171e7ca8727f2661f84ad6f98
jordancrawfordnz <jordan.crawford@me.com>
2014-10-12 20:33:34 +1300

    Implement OkText
    Read from a buffered reader into a piped writer, which is connected to a
    piped input reader which has a buffered reader (which is used by the
    command)

--- src/com/sneakyrocket/pocketrocket/v1/core/response/OkText.java
+++ src/com/sneakyrocket/pocketrocket/v1/core/response/OkText.java
@@ -1,20 +1,39 @@
 package com.sneakyrocket.pocketrocket.v1.core.response;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PipedReader;
+import java.io.PipedWriter;
 
 import com.sneakyrocket.pocketrocket.v1.core.Connection;
 
 public class OkText extends Response {
 	private BufferedReader reader;
+	private PipedReader pipedReader;
+	private PipedWriter pipedWriter;
 	
 	public OkText(String args, Connection connection)
 	{
 		super(args, connection);
+		pipedReader = new PipedReader();
+		reader = new BufferedReader(pipedReader);
 	}
 
 	@Override
 	public void process() {
-		// TODO Read text from connection, then finish
+		try {
+			BufferedReader incoming = connection.getNetworkInput();
+			pipedWriter = new PipedWriter(pipedReader);
+			
+			while(true)
+			{
+				int incomingChar = incoming.read();
+				if(incomingChar == 27) break;
+				pipedWriter.write(incomingChar);
+			}
+		} catch (IOException e) {
+			throw new ResponseFailureException();
+		}
 		super.process();
 	}
 	
