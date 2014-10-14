ae41bb2a6d8fdecdf9b3e8ee29e76afa310d31ac
jordancrawfordnz <jordan.crawford@me.com>
2014-10-14 15:07:50 +1300

    Implement ListScripts

--- src/com/sneakyrocket/pocketrocket/v1/core/command/ListScripts.java
+++ src/com/sneakyrocket/pocketrocket/v1/core/command/ListScripts.java
@@ -2,7 +2,15 @@ package com.sneakyrocket.pocketrocket.v1.core.command;
 
 import com.sneakyrocket.pocketrocket.v1.core.Connection;
 import com.sneakyrocket.pocketrocket.v1.core.Script;
-import java.util.List;
+import com.sneakyrocket.pocketrocket.v1.core.Session;
+import com.sneakyrocket.pocketrocket.v1.core.response.Hello;
+import com.sneakyrocket.pocketrocket.v1.core.response.OkText;
+import com.sneakyrocket.pocketrocket.v1.core.response.Response;
+import com.sneakyrocket.pocketrocket.v1.core.response.ResponseHandler;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class ListScripts extends Command{
 	public ListScripts(Connection connection)
@@ -10,10 +18,39 @@ public class ListScripts extends Command{
 		super(connection);
 	}
 	
-	public List<Script> getSripts()
+	public ArrayList<Script> getSripts()
 	{
-		// TODO Implement
-		throw new UnsupportedOperationException();
+		// Send off the request to the server.
+		if(!connection.sendRequest("listScripts"))
+			throw new CommandFailureException();
+		ResponseHandler handler = ResponseHandler.getInstance();
+		Response firstResponse = handler.getResponse(connection);
+		
+		Session currentSession = Session.getCurrentSession();
+		ArrayList<Script> scriptList = new ArrayList<Script>();
+		
+		if(firstResponse instanceof OkText)
+		{
+			OkText response = (OkText) firstResponse;
+			BufferedReader textOutput = response.getReader();
+			firstResponse.process();
+			String line;
+			
+			try
+			{
+				while((line = textOutput.readLine()) != null)
+				{
+					scriptList.add(new Script(currentSession,line));
+				}
+			}
+			catch(IOException ex)
+			{
+				throw new CommandFailureException();
+			}
+		}
+		else
+			throw new CommandFailureException();
+		return scriptList;
 	}
 	
 }
