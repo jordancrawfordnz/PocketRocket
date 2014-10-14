74082e25099309f4c1b48699afea714491dbd3ee
Andrew W <andrewjw1995@gmail.com>
2014-10-14 17:39:27 +1300

    Revert "Added debugging to OkText processing method"
    This reverts commit 91303e4ff359b0beee226bd2fe7950f730f86e69.

--- src/com/sneakyrocket/pocketrocket/v1/core/response/OkText.java
+++ src/com/sneakyrocket/pocketrocket/v1/core/response/OkText.java
@@ -5,11 +5,7 @@ import java.io.IOException;
 import java.io.PipedReader;
 import java.io.PipedWriter;
 
-import android.util.Log;
-
-import com.sneakyrocket.pocketrocket.R;
 import com.sneakyrocket.pocketrocket.v1.core.Connection;
-import com.sneakyrocket.pocketrocket.v1.core.GlobalApplication;
 
 public class OkText extends Response {
 	private BufferedReader reader;
@@ -37,8 +33,6 @@ public class OkText extends Response {
 			}
 			pipedWriter.close();
 		} catch (IOException e) {
-			Log.d(GlobalApplication.getInstance().getResources()
-					.getString(R.string.app_name), e.getMessage());
 			throw new ResponseFailureException();
 		}
 		super.process();
